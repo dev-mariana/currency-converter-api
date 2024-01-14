f0d0d0bc66f137548cc0c1619d78b914a801006c
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-01-13 23:59:20 -0300

    feat: import database module at currencies module

--- src/application/currencies/currencies.module.ts
+++ src/application/currencies/currencies.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/infra/database.module';
 import { CurrenciesController } from './controllers/currencies.controller';
 import { CurrenciesRepository } from './repositories/currencies.repository';
 import { CurrenciesService } from './services/currencies.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [CurrenciesController],
   providers: [CurrenciesService, CurrenciesRepository],
 })
