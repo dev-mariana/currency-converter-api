22b5e05e1595ffb492d448cc10fbd1812f918c6f
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-01-13 23:28:53 -0300

    feat: add http service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CurrenciesModule } from './application/currencies/currencies.module';
+import { HttpService } from './application/http/http.service';
 import { DatabaseModule } from './infra/database.module';
 
 @Module({
   imports: [DatabaseModule, CurrenciesModule],
   controllers: [],
-  providers: [],
+  providers: [HttpService],
 })
 export class AppModule {}
