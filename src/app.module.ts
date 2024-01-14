5fc13f12bd96ee090c3ccfc1e32928423375d875
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-01-13 23:40:07 -0300

    chore: add nestjs/config and import config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CurrenciesModule } from './application/currencies/currencies.module';
 import { HttpService } from './application/http/http.service';
 import { DatabaseModule } from './infra/database.module';
 
 @Module({
-  imports: [DatabaseModule, CurrenciesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    CurrenciesModule,
+  ],
   controllers: [],
   providers: [HttpService],
 })
