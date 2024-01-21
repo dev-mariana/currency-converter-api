64e4bc3d4b70d049839fe37a705a54f42e30b1a4
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-01-21 18:02:22 -0300

    feat: add axios request

--- src/application/http-client/services/http-client.service.ts
+++ src/application/http-client/services/http-client.service.ts
@@ -1,19 +1,18 @@
-import { HttpService } from '@nestjs/axios';
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { AxiosResponse } from 'axios';
-import { Observable } from 'rxjs';
+import axios from 'axios';
 import { ApiResponse } from '../entities/api-response';
 
 @Injectable()
 export class HttpClientService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor() {}
 
-  getCurrency(from: string): Observable<AxiosResponse<ApiResponse[]>> {
+  async getCurrency(from: string): Promise<ApiResponse> {
     const url = process.env.BASE_URL;
     const api_key = process.env.API_KEY;
 
     try {
-      return this.httpService.get(`${url}/${api_key}/latest/${from}`);
+      const response = await axios.get(`${url}/${api_key}/latest/${from}`);
+      return response.data;
     } catch (error) {
       throw new NotFoundException(error);
     }
