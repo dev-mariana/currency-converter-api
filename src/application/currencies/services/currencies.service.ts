a6242607c806fc41baa710c03b0b0d5367414c1d
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-01-23 12:03:17 -0300

    feat: createCurrency function at currencies service

--- src/application/currencies/services/currencies.service.ts
+++ src/application/currencies/services/currencies.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { HttpClientService } from 'src/application/http-client/services/http-client.service';
 import { ParamsDTO } from '../dto/params.dto';
+import { CurrencyEntity } from '../entities/currency';
+import { Currency } from '../interfaces/currency.interface';
 import { CurrenciesRepository } from '../repositories/currencies.repository';
 
 @Injectable()
@@ -19,10 +21,22 @@ export class CurrenciesService {
     const conversionRate = exchangeRates[to];
 
     if (!conversionRate) {
-      throw new Error('Invalid Currency.');
+      throw new Error('Invalid currency.');
     }
 
-    return amount * conversionRate;
+    const convertedAmount = amount * conversionRate;
+
+    const currency = {
+      code: response.base_code,
+      from,
+      to,
+      amount: Number(amount),
+      result: convertedAmount,
+    };
+
+    await this.createCurrency(currency);
+
+    return convertedAmount;
   }
 
   private normalizeParams({ from, to }: ParamsDTO): string[] {
@@ -30,5 +44,13 @@ export class CurrenciesService {
     const normalizedTo = to.toUpperCase();
 
     return [normalizedFrom, normalizedTo];
+  }
+
+  async createCurrency(data: Currency): Promise<CurrencyEntity> {
+    try {
+      return await this.currenciesRepository.create(data);
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
