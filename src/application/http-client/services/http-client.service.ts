010df1a0dbbcecd264bc9a4f72e490ad9676a133
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-01-23 13:00:58 -0300

    refactor: remove unused constructor

--- src/application/http-client/services/http-client.service.ts
+++ src/application/http-client/services/http-client.service.ts
@@ -4,14 +4,12 @@ import { ApiResponse } from '../entities/api-response';
 
 @Injectable()
 export class HttpClientService {
-  constructor() {}
-
   async getCurrency(from: string): Promise<ApiResponse> {
     const url = process.env.BASE_URL;
     const api_key = process.env.API_KEY;
 
     try {
-      const response = await axios.get(`${url}/${api_key}/latest/${from}`);
+      const response = await axios.get(`${url}/v6/${api_key}/latest/${from}`);
       return response.data;
     } catch (error) {
       throw new NotFoundException(error);
